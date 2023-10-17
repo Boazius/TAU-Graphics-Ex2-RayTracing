22d5ea4f72cbcc53d7aa75da0658dac98f855107
Boaz Yakubov <Boazyakubov@gmail.com>
2023-10-17 10:28:39 +0300

    linted main.py

--- main.py
+++ main.py
@@ -1,45 +1,51 @@
 import raytracer
 import sys
 
+# Set default width and height values
+DEFAULT_WIDTH = 500
+DEFAULT_HEIGHT = 500
 
-# The code should run in the command line (you need to send a directory with
-# all the python code files) and accept 4 parameters. For example:
-# python RayTracer.py scenes\Spheres.txt scenes\Spheres.png 500 500
-# those final two are optional, default value is 500x500
+
 def main():
+    # Initialize the RayTracer object
     ray_tracer = raytracer.RayTracer()
+
+    # Initialize scene_file_name and output_file_name variables
     scene_file_name = ""
     output_file_name = ""
 
-    # count expected args and throw exception
+    # Check the number of arguments provided through the command line
     if len(sys.argv) < 3:
-        print("not enough arguments")
+        print("Error: Insufficient arguments provided.")
         return
-    if len(sys.argv) == 3:
-        # Default width and height are 500x500
-        # width = 500
-        # height = 500
+    elif len(sys.argv) == 3:
         scene_file_name = sys.argv[1]
         output_file_name = sys.argv[2]
-    if len(sys.argv) == 4:
-        print("input 2 arguments or 4")
+    elif len(sys.argv) == 4:
+        print("Error: invalid arguments provided.")
         return
-    if len(sys.argv) == 5:
-        # We got optional arguments
-        width = int(sys.argv[3])
-        height = int(sys.argv[4])
-        ray_tracer.image_width = width
-        ray_tracer.image_height = height
-        scene_file_name = sys.argv[1]
-        output_file_name = sys.argv[2]
-    if len(sys.argv) > 5:
-        print("too many arguments")
+    elif len(sys.argv) == 5:
+        # Accept optional width and height arguments
+        try:
+            width = int(sys.argv[3])
+            height = int(sys.argv[4])
+            ray_tracer.image_width = width
+            ray_tracer.image_height = height
+            scene_file_name = sys.argv[1]
+            output_file_name = sys.argv[2]
+        except ValueError:
+            print("Error: Width and height must be integer values.")
+            return
+    else:
+        print("Error: Too many arguments provided.")
         return
 
+    # Parse the scene from the specified file
     ray_tracer.parseScene(scene_file_name)
+
+    # Render the scene and save the output image
     ray_tracer.renderScene(output_file_name)
 
 
-# Main Function Run
 if __name__ == '__main__':
     main()
